727571f5e9c61ad47752f9ffe31413921e73f533
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-10-26 21:47:06 -0300

    chore: simple calc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [result, setResult] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Valor de a: <input value={a} onChange={e => setA(e.target.value)} /><br />
+      Valor de b: <input value={b} onChange={e => setB(e.target.value)} /><br />
+      <button onClick={() => setResult(Number(a) + Number(b))}>Somar</button><br />
+      Resultado da soma é <h1>{result}</h1>
+    </>
   );
 }
 
